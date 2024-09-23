6239307d7682b71fb1a837db3ba45acfb73e8b63
Sharjeel Faiq <mrmalik610@gmail.com>
2024-09-23 22:43:44 +0500

    Refactor AccordionItem component to filter and render specific menu items

--- src/components/About/AccordionItem.jsx
+++ src/components/About/AccordionItem.jsx
@@ -4,19 +4,20 @@ import { dropdownOptionsProps } from "../DropdownMenu/DropdownOptionProps.js";
 const AccordionItems = ({ menu }) => {
   const manipulationBtnsProps = dropdownOptionsProps();
 
+  const filteredItems = manipulationBtnsProps.filter(
+    (item) => item.menuName === menu
+  );
+
   return (
     <ul style={{ listStyleType: "none" }} className="lh-lg">
-      {manipulationBtnsProps.map(
-        (item, index) =>
-          item.menuName === menu && (
-            <li key={index} style={{ width: "80%", borderRadius: "5px" }}>
-              <small>
-                <strong>{item.optionName} - </strong>
-                {item.description}
-              </small>
-            </li>
-          )
-      )}
+      {filteredItems.map((item, index) => (
+        <li key={index} style={{ width: "80%", borderRadius: "5px" }}>
+          <small>
+            <strong>{item.optionName} - </strong>
+            {item.description}
+          </small>
+        </li>
+      ))}
     </ul>
   );
 };
