e61115fc0ef887ee6241e463d0715f32cd4dbd17
Sharjeel Faiq <mrmalik610@gmail.com>
2024-10-01 16:33:51 +0500

    Fix import path for QuotesData.js in DropdownMenuFunctions.js

--- src/components/DropdownMenu/DropdownMenuFunctions.js
+++ src/components/DropdownMenu/DropdownMenuFunctions.js
@@ -33,7 +33,7 @@ import {
   unixToDate,
   unixToTime,
 } from "number-pro";
-import { getQuote } from "../Utils/QuotesData";
+import { getQuote } from "../Utils/QuotesData.js";
 import {
   alternateCase1,
   alternateCase2,
